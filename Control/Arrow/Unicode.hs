a9b71287a313b21414b7ea2198776f9163ce8304
Roel van Dijk <vandijk.roel@gmail.com>
2011-03-30 13:03:06 +0200

    Maintenance
    Ignore-this: e3931aea75a2463562efc57e7f83e26e
    Made compatible with GHC-6.8.1 and up
    Updated copyright year
    Changed version from 0.2.1.1 to 0.2.1.2
    
    darcs-hash:20110330110306-cab80-a946b09181866b333d94ee86093478bec709b083.gz

--- Control/Arrow/Unicode.hs
+++ Control/Arrow/Unicode.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude, UnicodeSyntax, TypeOperators #-}
+{-# LANGUAGE CPP, NoImplicitPrelude, UnicodeSyntax, TypeOperators #-}
 
 {-|
 Module     : Control.Arrow.Unicode
@@ -7,7 +7,15 @@ License    : BSD3 (see the file LICENSE)
 Maintainer : Roel van Dijk <vandijk.roel@gmail.com>
 -}
 
-module Control.Arrow.Unicode ( (⋙), (⋘), (⁂), (⧻), (⫴) ) where
+module Control.Arrow.Unicode
+    ( (⋙)
+    , (⋘)
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ > 608
+    , (⁂)
+#endif
+    , (⧻)
+    , (⫴)
+    ) where
 
 
 -------------------------------------------------------------------------------
@@ -18,23 +26,33 @@ module Control.Arrow.Unicode ( (⋙), (⋘), (⁂), (⧻), (⫴) ) where
 import Control.Arrow ( Arrow, (***), ArrowChoice, (+++), (|||) )
 import Data.Either   ( Either )
 
+#if MIN_VERSION_base(3,0,3)
 -- from base-unicode-symbols
 import Control.Category.Unicode ( (⋙), (⋘) )
+#else
+import Control.Arrow ( (>>>), (<<<) )
+#endif
 
 
 -------------------------------------------------------------------------------
 -- Fixities
 -------------------------------------------------------------------------------
 
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ > 608
 infixr 3 ⁂
+#endif
 infixr 2 ⧻
 infixr 2 ⫴
+#if !MIN_VERSION_base(3,0,3)
+infixr 1 ⋙, ⋘
+#endif
 
 
 -------------------------------------------------------------------------------
 -- Symbols
 -------------------------------------------------------------------------------
 
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ > 608
 {-|
 (&#x2042;) = ('***')
 
@@ -42,6 +60,7 @@ U+2042, ASTERISM
 -}
 (⁂) ∷ Arrow (⇝) ⇒ (α ⇝ β) → (α' ⇝ β') → (α, α') ⇝ (β, β')
 (⁂) = (***)
+#endif
 
 {-|
 (&#x29FB;) = ('+++')
@@ -58,3 +77,21 @@ U+2AF4, TRIPLE VERTICAL BAR BINARY RELATION
 -}
 (⫴) ∷ ArrowChoice (⇝) ⇒ (α ⇝ δ) → (β ⇝ δ) → (Either α β ⇝ δ)
 (⫴) = (|||)
+
+#if !MIN_VERSION_base(3,0,3)
+{-|
+(&#x22D9;) = ('>>>')
+
+U+22D9, VERY MUCH GREATER-THAN
+-}
+(⋙) ∷ Arrow (⇝) ⇒ (α ⇝ β) → (β ⇝ γ) → (α ⇝ γ)
+(⋙) = (>>>)
+
+{-|
+(&#x22D8;) = ('<<<')
+
+U+22D8, VERY MUCH LESS-THAN
+-}
+(⋘) ∷ Arrow (⇝) ⇒ (β ⇝ γ) → (α ⇝ β) → (α ⇝ γ)
+(⋘) = (<<<)
+#endif
