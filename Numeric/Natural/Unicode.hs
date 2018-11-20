f9b8632aa6b9bb9d2197c4682f10106f1e9b2175
Roel van Dijk <roel@lambdacube.nl>
2018-11-20 11:57:52 +0100

    Add U+2115, DOUBLE-STRUCK CAPITAL N
    Closes issue #18

---
+++ Numeric/Natural/Unicode.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE NoImplicitPrelude, UnicodeSyntax #-}
+
+module Numeric.Natural.Unicode ( ℕ ) where
+
+import Numeric.Natural ( Natural )
+
+{-|
+U+2115, DOUBLE-STRUCK CAPITAL N
+-}
+type ℕ = Natural
